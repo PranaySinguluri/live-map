a6e535d981cbc1ead677ca4e58178290bcd80c29
Pranay Singuluri <singuluripranay20@gmail.com>
2026-05-26 18:00:17 -0500

    added url

--- backend/src/main/java/com/livemap/LiveMapApplication.java
+++ backend/src/main/java/com/livemap/LiveMapApplication.java
@@ -2,8 +2,10 @@ package com.livemap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.livemap"})
 public class LiveMapApplication {
     public static void main(String[] args) {
         SpringApplication.run(LiveMapApplication.class, args);
